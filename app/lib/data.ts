f48a7e11861923912a640fd40130dcea608c8425
Victor Pavlov <vitekpavlov@gmail.com>
2023-10-30 11:18:32 +0200

    Chapter 9 passed.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -36,6 +36,11 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    // Artificially delay a reponse for demo purposes.
+    // Don't do this in real life :)
+
+    console.log('Fetching latest invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
